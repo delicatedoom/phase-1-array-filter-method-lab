dff5d97cc5ef663e248e7b7e15d0865e372bb864
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-06-15 08:09:46 -0600

    commit tree 0199eb0c5b0e68e8a1a9c450ca8f6879d70fcdb6

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Danny',
+      hometown: 'Frankfurt' },
+    {
+      name: 'Marc',
+      hometown: 'Missoula' } ,
+    {
+      name: 'Elise',
+      hometown: 'Seattle' },
+    {
+      name: 'Troy',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Danny',
+      hometown: 'Pheonix' }
+  ];
+
+function findMatching(array, name){
+    return array.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(array, letters){
+    let string = letters.length
+    return array.filter(person => (person.slice(0, string)) === letters)
+}
+
+function matchName(array, whoIsIt){
+    return array.filter(element => element.name === whoIsIt)
+}
